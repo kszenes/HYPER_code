95a2f9689db4ecb0d6c68773231ae4eb8158cbea
szeneskalman <szenes.kalman@gmail.com>
2021-11-23 00:22:28 +0100

    added connectivity calculation and hypergraph class

--- hg_tools/src/io/partitioning.cpp
+++ hg_tools/src/io/partitioning.cpp
@@ -3,6 +3,29 @@
 #include <string>
 #include <tuple>
 #include <random>
+#include <algorithm>
+
+
+class Hypergraph {
+
+
+
+  public:
+    void print_edges() {
+      for (int i = 0; i < num_rows; i++) {
+        for (auto j : edges[i]) {
+          std::cout << j << " ";
+        }
+        std::cout << std::endl;
+      }
+    }
+
+    int num_rows = 0;
+    int num_cols = 0;
+    int num_nodes = 0;
+    std::shared_ptr<double[]> matrix;
+    std::vector<std::vector<double>> edges;
+};
 
 /**
  * @brief Reads mtx file
@@ -10,7 +33,7 @@
  * @param filename 
  * @return tuple containing number of rows, cols and matrix
  */
-std::tuple<int, int, std::unique_ptr<double[]>> read_mtx(const std::string& filename) {
+void mtx2matrix(const std::string& filename, Hypergraph &graph) {
   std::ifstream file(filename);
   int num_row, num_col, num_lines;
 
@@ -18,10 +41,11 @@ std::tuple<int, int, std::unique_ptr<double[]>> read_mtx(const std::string& file
   while (file.peek() == '%') file.ignore(2048, '\n');
 
   // Read number of rows and columns
-  file >> num_row>> num_col >> num_lines;
+  file >> num_row >> num_col >> num_lines;
 
   // Create 2D array and fill with zeros
-  std::unique_ptr<double[]> matrix(new double[num_row * num_col]);
+  std::shared_ptr<double[]> matrix(new double[num_row * num_col]);
+  std::vector<std::vector<double>> edges(num_row);
 
   // fill the matrix with data
   double data = 1.0;
@@ -30,11 +54,17 @@ std::tuple<int, int, std::unique_ptr<double[]>> read_mtx(const std::string& file
       int row, col;
       file >> row >> col;
       matrix[(row -1) + (col -1) * num_row] = data;
+      edges[row-1].push_back(col-1);
   }
 
   file.close();
-  return {num_row, num_col, std::move(matrix)};
+  graph.num_cols = num_col;
+  graph.num_rows = num_row;
+  graph.matrix = matrix;
+  graph.edges = edges;
+  graph.num_nodes = num_lines;
 }
+
 
 /**
  * @brief Creates epsilon balanced random partition
@@ -44,7 +74,7 @@ std::tuple<int, int, std::unique_ptr<double[]>> read_mtx(const std::string& file
  * @param num_vertices 
  * @param condition: max amount of vertex allowed in cluster (for epsilon-balanced partitioning)
  */
-void random_partitions(std::vector<int> part[], int num_partitions, int num_vertices, int condition) {
+void random_partitions(std::vector<std::vector<int>> &part, int num_partitions, int num_vertices, int condition) {
   std::random_device rand_dev;
   std::mt19937 generator(rand_dev());
   std::uniform_int_distribution<int> distr(0, num_partitions-1);
@@ -77,14 +107,48 @@ double compute_condition(int num_vertices, int num_partitions, double epsilon) {
   return (1. + epsilon) * num_vertices / num_partitions;
 }
 
+void compute_connectivity(std::vector<std::vector<int>> &part, Hypergraph &graph){
+  int cost = 0;
+
+  for (int i = 0; i < graph.num_rows; i++) {
+    std::vector<double> lambda;
+    for (auto j : graph.edges[i]) {
+      for (int k = 0; k < part.size(); k++) {
+        if (std::find(part[k].begin(), part[k].end(), j) != part[k].end()) {
+          if (std::find(lambda.begin(), lambda.end(), k) != lambda.end()) {
+          } else {
+            cost++;
+            // std::cout << "cost increased" << std::endl;
+            lambda.push_back(k);
+          }
+          break;
+      }
+
+      }
+    }
+  }
+
+  std::cout << "Cost = " << cost - graph.num_rows << std::endl;
+
+}
+
 int main(int argc, char* argv[]) {
   if (argc < 2) {
     std::cerr << "Please provide .mtx file (e.g. $ ./a.out ../../../HYPER_PUBLIC/bcsstk21.mtx)" << std::endl;
     return 0;
   }
-  int num_row, num_col;
-  std::unique_ptr<double[]> matrix;
-  std::tie(num_row, num_col, matrix) = read_mtx(argv[1]);
+  // std::unique_ptr<double[]> matrix;
+  // std::tie(num_row, num_col, matrix) = mtx2matrix(argv[1]);
+
+  Hypergraph graph;
+
+  std::vector<std::vector<double>> hmetis;
+  mtx2matrix(argv[1], graph);
+  int num_rows = graph.num_rows;
+  int num_cols = graph.num_cols;
+  int num_nodes = graph.num_nodes;
+
+  // graph.print_edges();
 
   // Print out matrix; rows = hyperedges, cols = vertices
   /* for (int row = 0; row < num_row; row++) {
@@ -94,25 +158,26 @@ int main(int argc, char* argv[]) {
      std::cout << std::endl;
    }*/
 
-  std::cout << "Num Edges (rows): " << num_row << ";  Num Vertices (cols): " << num_col << std::endl;
+  std::cout << "Num Edges (rows): " << num_rows << ";  Num Vertices (cols): " << num_cols << "; Num nodes (lines): " << num_nodes << std::endl;
 
   int num_partitions = 2;
   double epsilon = 0.2;
-  int condition = compute_condition(num_col, num_partitions, epsilon);
+  int condition = compute_condition(num_cols, num_partitions, epsilon);
 
   std::cout << "Clusters can have at most " << condition << " vertices!" << std::endl;
 
-  std::vector<int> part[num_partitions]; 
+  std::vector<std::vector<int>> part(num_partitions); 
 
-  random_partitions(part, num_partitions, num_col, condition);
+  random_partitions(part, num_partitions, num_cols, condition);
+  compute_connectivity(part, graph);
 
-  for (int i = 0; i < num_partitions; i++) {
-    std::cout << "\nCluster " << i << " has " << part[i].size() << " vertices" << std::endl;
-    for (auto j : part[i]) {
-      std::cout << j << " ";
-    }
-    std::cout << std::endl;
-  }
+  // for (int i = 0; i < num_partitions; i++) {
+  //   std::cout << "\nCluster " << i << " has " << part[i].size() << " vertices" << std::endl;
+  //   for (auto j : part[i]) {
+  //     std::cout << j << " ";
+  //   }
+  //   std::cout << std::endl;
+  // }
 
 
 
